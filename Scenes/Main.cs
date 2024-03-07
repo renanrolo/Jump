4e4f06c20388200310a4e134adfd4bd0c5ead3a4
Renan Rolo <renanrolo@hotmail.com>
2024-03-07 13:31:25 -0400

    start

---
+++ Scenes/Main.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Main : Node2D
+{
+	private Player player;
+	private Label playerX;
+	private Label playerY;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		player = GetNode<Player>("Player");
+		var startPosition = GetNode<Marker2D>("StartPosition");
+		player.Start(startPosition.Position);
+		playerX = GetNode<Label>("VelocityX");
+		playerY = GetNode<Label>("VelocityY");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		playerX.Text = "X: " + player.Velocity.X;
+		playerY.Text = "Y: " + player.Velocity.Y;
+	}
+}
